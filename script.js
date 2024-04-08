70c24616e6e6e6e6fe0f6f27dcb8bf81939bedb6
olhos.do.abismo <122611368+gabriel0138@users.noreply.github.com>
2024-04-08 15:35:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function start() {
+    window.location.href = "vales.html";
+}
+
+function redeem(type) {
+    if (type === 'beijo','testa') {
+        window.location.href = "ticket.html"; // Redireciona para a página de resgate do Vale Beijo
+    } else {
+        // Adicione aqui a lógica para redirecionar para outras páginas de resgate
+    }
+}
